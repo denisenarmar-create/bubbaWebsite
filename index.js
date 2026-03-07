dcadda64c0fca70903419a5f598a03b3eb52549b
denisenarmar-create <denisenarmar@gmail.com>
2026-03-07 13:25:27 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function isMobile() {
+    return /mobi|Android|iphone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+const title = document.getElementById('mainTitle');
+
+
+const text1 = document.getElementById('para');
+
+if (text1.style.textAlign = 'center') {
+    text1.style.textAlign = 'none';
+}
